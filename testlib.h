a0fa9aeb370ee789952f90816fdc34bd1f97273c
13bscsmrafi <13bscsmrafi@seecs.edu.pk>
2016-02-12 16:38:23 +0500

    Create testlib.h
    testcase file

---
+++ testlib.h
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int testcase(int l, int m, int z, int n)
+{
+	if (m != z || z != m){
+		cout << "Wrong dimensions for multiplications" << endl<<"Run program again"<<endl;
+		return 1;
+	}
+}
